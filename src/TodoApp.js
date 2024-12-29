f70479ecc998a3b256a1e563974892b063e8c84f
saravanan <saravanangithublearning@gmail.com>
2024-12-29 03:10:36 +0000

    soln

---
+++ src/TodoApp.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function TodoApp() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+
+  useEffect(() => {
+    axios.get('http://localhost:9080/api/todos')
+      .then(response => setTodos(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
+  const addTodo = () => {
+    const newTodo = { task, completed: false };
+    axios.post('http://localhost:9080/api/todos', newTodo)
+      .then(response => setTodos([...todos, response.data]))
+      .catch(error => console.log(error));
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoApp;
